afc4dd8cdd46c05fc8622426fd1dbd499dda642b
jaime <jaimecvr96@gmail.com>
2023-04-13 09:08:30 +0200

    Initial commit

---
+++ src/test/kotlin/com/reactorsolutions/holog/HologApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.reactorsolutions.holog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HologApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
